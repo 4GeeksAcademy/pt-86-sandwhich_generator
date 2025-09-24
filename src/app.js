399e6af5e9b83e535852716bc1c4d37565311adc
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2025-09-24 22:51:40 +0000

    many yummy sandwhiches

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".btn").addEventListener("click", 
+    generateSandwhich)
 };
+
+const generateSandwhich = () => {
+  let breads = ["white", "wheat", "rye", "potato", "Hawaiian", "Italian"];
+  let meats = ["turkey", "tuna", "chicken", "pepperoni", "ham", "meatball"];
+  let cheeses = ["American", "Swiss", "Pepper Jack", "Provolone", "Cheddar"];
+  let sauces = ["Mayo", "Mustard", "Ranch", "Oil & Vinegar", "Ketchup"];
+  let vegs = ["lettuce", "spinach", "Onions", "bell peppers", "tomato"];
+
+  let breadIndex = Math.floor(Math.random() * breads.length);
+  let meatIndex = Math.floor(Math.random() * meats.length);
+  let cheeseIndex = Math.floor(Math.random() * cheeses.length);
+  let sauceIndex = Math.floor(Math.random() * sauces.length);
+  let vegIndex = Math.floor(Math.random() * vegs.length);
+
+  let breadDiv = document.querySelector("#bread");
+  let meatDiv = document.querySelector("#meat");
+  let cheeseDiv = document.querySelector("#cheese");
+  let sauceDiv = document.querySelector("#sauce");
+  let vegDiv = document.querySelector("#veg");
+
+  breadDiv.innerHTML = breads[breadIndex];
+  meatDiv.innerHTML = meats[meatIndex];
+  cheeseDiv.innerHTML = cheeses[cheeseIndex];
+  sauceDiv.innerHTML = sauces[sauceIndex];
+  vegDiv.innerHTML = vegs[vegIndex];
+}
